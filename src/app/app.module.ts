25cec4a72ff39eb77bec8cca200dffe87f53a2c1
Dorcell <death_page@Mail.ru>
2021-10-21 18:00:20 +0700

    separate components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
 import { PersonsTableComponent } from './persons-table/persons-table.component';
+import { PersonModalComponent } from './person-modal/person-modal.component';
+
 import { ReactiveFormsModule } from "@angular/forms";
 import { HttpClient, HttpClientModule } from "@angular/common/http";
 
 @NgModule({
   declarations: [
     AppComponent,
-    PersonsTableComponent
+    HeaderComponent,
+    PersonsTableComponent,
+    PersonModalComponent
   ],
     imports: [
         BrowserModule,
-        AppRoutingModule,
         ReactiveFormsModule,
         HttpClientModule,
     ],
---
+++ src/app/person-modal/person-modal.component.ts
@@ -0,0 +1,67 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { ApiService } from "../shared/api.service";
+import { PersonModel } from "../persons-table/persons-table.model";
+import {Notify} from "notiflix/build/notiflix-notify-aio";
+
+@Component({
+  selector: 'app-person-modal',
+  templateUrl: './person-modal.component.html',
+  styleUrls: ['./person-modal.component.css']
+})
+export class PersonModalComponent implements OnInit {
+
+  formValue !: FormGroup;
+  personModelObj : PersonModel = new PersonModel();
+  personData !: any;
+  constructor(private formbuilder: FormBuilder, private  api: ApiService) { }
+
+  ngOnInit(): void {
+    this.formValue = this.formbuilder.group({
+      firstName: [''],
+      lastName: ['']
+    })
+  }
+
+  postPersonDetails() {
+    this.personModelObj.firstName = this.formValue.value.firstName;
+    this.personModelObj.lastName = this.formValue.value.lastName;
+
+    this.api.postPerson(this.personModelObj)
+      .subscribe(res => {
+          console.log(res);
+          Notify.success("Сотрудник успешно добавлен");
+          let ref = document.getElementById('cancel');
+          ref?.click();
+          this.formValue.reset();
+          this.getAllPersons();
+        },
+        err => {
+          Notify.failure("Ошибка!");
+        })
+  }
+
+  updatePersonDetails() {
+    this.personModelObj.firstName = this.formValue.value.firstName;
+    this.personModelObj.lastName = this.formValue.value.lastName;
+
+    this.api.putPerson(this.personModelObj, this.personModelObj.id)
+      .subscribe(res => {
+          Notify.success("Сотрудник успешно обновлен")
+          let ref = document.getElementById('cancel');
+          ref?.click();
+          this.formValue.reset();
+          this.getAllPersons();
+        },
+        err => {
+          Notify.failure("Ошибка!");
+        })
+  }
+
+  getAllPersons() {
+    this.api.getPerson()
+      .subscribe(res => {
+        this.personData = res;
+      })
+  }
+}
